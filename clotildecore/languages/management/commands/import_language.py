3c8cbd1be49c1bc495e19bdf826e616f485e22c9
Chiara Paci <chiara.paci@gmail.com>
2019-02-20 14:05:54 +0100

    Import language with clean

--- clotildecore/languages/management/commands/import_language.py
+++ clotildecore/languages/management/commands/import_language.py
@@ -62,8 +62,10 @@ class Command(BaseCommand):
         fd=archive.extractfile(m_non_words)
         non_words=json.loads(fd.read().decode())
         for k in non_words:
-            models.NonWord.objects.get_or_create(language=language,word=non_words[k],
-                                                 defaults={"name": k})
+            w,created=models.NonWord.objects.get_or_create(language=language,word=non_words[k],
+                                                           defaults={"name": k})
+            w.name=k
+            w.save()
 
         pos_dict={}
         desc_dict={}
@@ -84,12 +86,15 @@ class Command(BaseCommand):
         data=json.loads(fd.read().decode())
         for name in data:
             desc,created=base_models.Description.objects.get_or_create(name=name)
+            ok=[]
             desc_dict[name]=desc
             for k in data[name]:
                 attr,created=base_models.Attribute.objects.get_or_create(name=k)
                 val,created=base_models.Value.objects.get_or_create(string=data[name][k])
                 entry,created=base_models.Entry.objects.get_or_create(attribute=attr,value=val)
+                ok.append(entry.pk)
                 desc.entries.add(entry)
+            desc.entries.remove(*desc.entries.exclude(pk__in=ok))
 
         # temas
         tarinfo=archive.getmember("./temas.json")
@@ -98,16 +103,19 @@ class Command(BaseCommand):
         for name in data:
             tema,created=morph_models.Tema.objects.get_or_create(name=name)
             tema_dict[name]=tema
+            ok=[]
             for k in data[name]:
                 attr,created=morph_models.TemaArgument.objects.get_or_create(name=k)
                 val,created=morph_models.TemaValue.objects.get_or_create(name=data[name][k])
                 entry,created=morph_models.TemaEntry.objects.get_or_create(argument=attr,value=val,tema=tema)
-
+                ok.append(entry.pk)
+            morph_models.TemaEntry.objects.filter(tema=tema).exclude(pk__in=ok).delete()
 
         # roots
         tarinfo=archive.getmember("./roots.json")
         fd=archive.extractfile(tarinfo)
         data=json.loads(fd.read().decode())
+        root_ok=[]
         for root in data:
             tema=tema_dict[root["tema"]]
             pos=pos_dict[root["part_of_speech"]]
@@ -116,10 +124,11 @@ class Command(BaseCommand):
             obj,created=morph_models.Root.objects.get_or_create(root=val,part_of_speech=pos,
                                                                 tema_obj=tema,description_obj=desc,
                                                                 language=language)
+            root_ok.append(obj.pk)
 
         # paradigmas
-
         par_dict={}
+        par_ok=[]
         for tarinfo in archive.getmembers():
             if not tarinfo.name.startswith('./paradigmas/'): continue
             fd=archive.extractfile(tarinfo)
@@ -128,7 +137,9 @@ class Command(BaseCommand):
             par,created=morph_models.Paradigma.objects.get_or_create(name=data["name"],
                                                                      defaults={"language":language,
                                                                                "part_of_speech":pos})
+            par_ok.append(par.pk)
             par_dict[data["name"]]=par
+            ok=[]
             for infl in data["inflections"]:
                 desc=desc_dict[infl["description"]]
                 regsub,created=morph_models.RegexpReplacement.objects.get_or_create(pattern=infl["regsub"][0],
@@ -137,11 +148,14 @@ class Command(BaseCommand):
                 obj.dict_entry=infl["dict_entry"]
                 obj.save()
                 par.inflections.add(obj)
+                ok.append(obj.pk)
+            par.inflections.remove(*par.inflections.exclude(pk__in=ok))
 
         # derivations
         tarinfo=archive.getmember("./derivations.json")
         fd=archive.extractfile(tarinfo)
         data=json.loads(fd.read().decode())
+        der_ok=[]
         for name in data:
             regsub,created=morph_models.RegexpReplacement.objects.get_or_create(pattern=data[name]["regsub"][0],
                                                                                 replacement=data[name]["regsub"][1])
@@ -166,15 +180,19 @@ class Command(BaseCommand):
             der.root_part_of_speech_obj=r_pos
             der.paradigma=par
             der.save()
+            der_ok.append(der.pk)
             
         # fusions
 
         tarinfo=archive.getmember("./fusions.json")
         fd=archive.extractfile(tarinfo)
         data=json.loads(fd.read().decode())
+        fusion_ok=[]
         for name in data:
             fusion,created=morph_models.Fusion.objects.get_or_create(name=name,language=language)
+            fusion_ok.append(fusion.pk)
             n=0
+            ok=[]
             for rule in data[name]:
                 regsub,created=morph_models.RegexpReplacement.objects.get_or_create(pattern=rule["regsub"][0],
                                                                                     replacement=rule["regsub"][1])
@@ -198,5 +216,14 @@ class Command(BaseCommand):
                 rel,created=morph_models.FusionRuleRelation.objects.get_or_create(fusion_rule=frule,
                                                                                   fusion=fusion,order=n)
                 n+=1
+                ok.append(rel.pk)
+
+            morph_models.FusionRuleRelation.objects.filter(fusion=fusion).exclude(pk__in=ok).delete()
+
+
+        morph_models.Fusion.objects.filter(language=language).exclude(pk__in=fusion_ok).delete()
+        morph_models.Root.objects.filter(language=language).exclude(pk__in=root_ok).delete()
+        morph_models.Derivation.objects.filter(language=language).exclude(pk__in=der_ok).delete()
+        morph_models.Paradigma.objects.filter(language=language).exclude(pk__in=par_ok).delete()
 
         archive.close()
